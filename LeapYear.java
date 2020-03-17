60c8933dd533099906638231b6d3570ffc47b81e
Njabulo <57357082+Njabulongwenyama@users.noreply.github.com>
2020-03-17 12:44:34 +0200

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,12 @@
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+
+        if(year <= 0 || year > 9999){
+            return false;
+        }else if (year % 4 != 0) {
+            return false;
+        } else if (year % 400 == 0) {
+            return true;
+        } else return year % 100 != 0;
+    }
+}
